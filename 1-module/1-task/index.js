4d3fa8c302b9cfcca9c2469a65db14a1092afd4f
Konstantin Pomazan <kospomazan@gmail.com>
2021-06-28 16:09:19 +0300

    factorial function has been completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  let counter = n;
+  for (let i = 0; i < n; i++) {
+    if (n <= 1) {
+      return 1;
+    }
+    result *= counter;
+    counter--;
+  }
+  return result;
 }
+
+factorial(5);
