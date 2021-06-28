ec5cd83cf7014e2edd4c177492887e634246e176
Konstantin Pomazan <kospomazan@gmail.com>
2021-06-28 20:29:45 +0300

    function checkSpam has been completed

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,13 @@
 function checkSpam(str) {
-  // ваш код...
+  const spamList = ['1XbeT', 'XXX', 'free', 'xxxxx'];
+  let lowerStr = str.toLowerCase();
+
+  for (let i = 0; i < spamList.length; i++) {
+    if (lowerStr.includes(spamList[i].toLowerCase())) {
+      return true;
+    } else {
+      continue;
+    }
+  }
+  return false;
 }
