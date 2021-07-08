17f77d07fa76b466a912b68628b4c10feaee3236
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-08 19:00:13 +0300

    added camelize task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,14 @@
 function camelize(str) {
-  // ваш код...
+  let result = null;
+  const arr = str.split("");
+
+  arr.forEach((elem, i) => {
+    if (elem.includes("-")) {
+      arr.splice(i, 1);
+      arr[i] = arr[i].toUpperCase();
+    }
+  });
+
+  result = arr.join("");
+  return result;
 }
