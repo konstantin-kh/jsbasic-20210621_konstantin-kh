447bbe914bb4876b7a0372a3340d57d579d01c63
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-09 13:24:17 +0300

    bug fixes for filterRange function

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,11 +1,9 @@
 function filterRange(arr, a, b) {
-   const found = [];
-   const target = [a, b - 1];
-
-   for (let i = 0; i <= arr.length + 1; i++) {
-     if (arr.includes(arr[target[i]])) {
-       found.push(arr[target[i]]);
-     }
-   }
-   return found;
+  const found = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      found.push(arr[i]);
+    }
+  }
+  return found;
 }
