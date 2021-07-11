d539dacb929b7f947e480a5313d373b00b8368eb
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-11 22:18:32 +0300

    updated camelize  function

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,14 +1,12 @@
 function camelize(str) {
-  let result = null;
-  const arr = str.split("");
+  const arr = str.split('');
 
-  arr.forEach((elem, i) => {
-    if (elem.includes("-")) {
+  let res = arr.map((elem, i) => {
+    if (elem === '-') {
       arr.splice(i, 1);
       arr[i] = arr[i].toUpperCase();
     }
   });
 
-  result = arr.join("");
-  return result;
+  return res = arr.join('');
 }
