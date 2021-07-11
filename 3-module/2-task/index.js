878e33a62414d5726ce593cb379591a0ac1536ae
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-11 18:33:48 +0300

    updated filterRange  function

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,9 +1,3 @@
 function filterRange(arr, a, b) {
-  const found = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= a && arr[i] <= b) {
-      found.push(arr[i]);
-    }
-  }
-  return found;
+  return arr.filter(elem => elem >= a && elem <= b);
 }
