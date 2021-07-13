53a32df14f224cfcb604d55e731e8825b54e80b2
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-13 18:37:23 +0300

    added showSalary function

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,27 @@
 function showSalary(users, age) {
-  // ваш код...
+  const foundUsers = [];
+  let list = "";
+
+  users.forEach((user) => {
+    if (user.age <= age) {
+      foundUsers.push(user.name);
+      foundUsers.push(user.balance);
+    }
+  });
+
+  if (foundUsers.length) {
+    foundUsers.forEach((elem, i, arr) => {
+      if (i === 0) {
+        list += `'${elem}, `;
+      } else if (i % 2 === 0) {
+        list += `${elem}, `;
+      } else if (i % 2 !== 0 && i !== arr.length - 1) {
+        list += `${elem}\n`;
+      } else {
+        list += `${elem}'`;
+      }
+    });
+  }
+
+  return list.slice(1, -1);
 }
