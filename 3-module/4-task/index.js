050351e1a0015ef503785ee42469c0447a7d3e4c
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-17 17:47:15 +0300

    updated showSalary function

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,25 +1,6 @@
 function showSalary(users, age) {
-  let list = "";
-
-  const foundUsers = users
-    .filter((user) => user.age <= age)
-    .map((user) => {
-      return [user.name, user.balance];
-    });
-
-  if (foundUsers.length) {
-    foundUsers.flat().forEach((elem, i, arr) => {
-      if (i === 0) {
-        list += `'${elem}, `;
-      } else if (i % 2 === 0) {
-        list += `${elem}, `;
-      } else if (i % 2 !== 0 && i !== arr.length - 1) {
-        list += `${elem}\n`;
-      } else {
-        list += `${elem}'`;
-      }
-    });
-  }
-
-  return list.slice(1, -1);
+  return users
+    .filter(user => user.age <= age)
+    .map(user => `${user.name}, ${user.balance}`)
+    .join('\n');
 }
