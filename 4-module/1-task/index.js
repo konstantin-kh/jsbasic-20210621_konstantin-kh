a6b589bed4da145edcec88feb0efa4c88d577cb8
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-17 22:31:45 +0300

    added makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement("ul");
+
+  friends.forEach(friend => {
+    const li = document.createElement("li");
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    ul.appendChild(li);
+  });
+
+  return ul;
 }
