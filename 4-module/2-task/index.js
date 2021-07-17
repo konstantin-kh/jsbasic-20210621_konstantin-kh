4b06d91e60da5f0eed03577aef337ddf15f58549
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-17 23:27:15 +0300

    added makeDiagonalRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,15 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i in table.rows) {
+    let row = table.rows[i];
+
+    for (let j in row.cells) {
+      let col = row.cells[i];
+
+      if (!(row[i] || col)) {
+        break;
+      } else {
+        col.style.backgroundColor = "red";
+      }
+    }
+  }
 }
