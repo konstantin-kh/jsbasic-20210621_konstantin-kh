17300efaa0e67d9ec69855276ecad39bfc5ad30a
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-18 21:30:41 +0300

    fix makeDiagonalRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,15 +1,11 @@
 function makeDiagonalRed(table) {
-  for (let i in table.rows) {
-    let row = table.rows[i];
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i];
 
-    for (let j in row.cells) {
-      let col = row.cells[i];
-
-      if (!(row[i] || col)) {
-        break;
-      } else {
-        col.style.backgroundColor = "red";
-      }
+    if (!(row[i] || row.cells[i])) {
+      break;
+    } else {
+      row.cells[i].style.backgroundColor = "red";
     }
   }
 }
