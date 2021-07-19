308f06dede44d37f5d625d4536e4dbfa7e767ffa
Konstantin Pomazan <kospomazan@gmail.com>
2021-07-19 18:57:34 +0300

    added highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,51 @@
 function highlight(table) {
-  // ваш код...
+  const row = table.querySelectorAll("tr");
+  let foundGender = null;
+  let foundAge = null;
+  let foundStatus = null;
+
+  for (let i = 0; i < row[0].children.length; i++) {
+    switch (row[0].children[i].innerText) {
+      case "Age":
+        foundAge = i;
+        break;
+
+      case "Gender":
+        foundGender = i;
+        break;
+
+      case "Status":
+        foundStatus = i;
+        break;
+    }
+  }
+
+  for (let j = 1; j < row.length; j++) {
+    const hasData = row[j].children[foundStatus];
+    const age = row[j].children[foundAge].innerText;
+    const gender = row[j].children[foundGender];
+
+    if (hasData.hasAttribute("data-available")) {
+      const checkData = hasData.getAttribute("data-available");
+
+      if (checkData === "true") {
+        hasData.parentElement.classList.add("available");
+      } else if (checkData === "false") {
+        hasData.parentElement.classList.add("unavailable");
+      }
+
+    } else {
+      hasData.parentElement.hidden = true;
+    }
+
+    if (age < 18) {
+      gender.parentElement.style = "text-decoration: line-through";
+    }
+
+    if (gender.innerText === "m") {
+      gender.parentElement.classList.add("male");
+    } else if (gender.innerText === "f") {
+      gender.parentElement.classList.add("female");
+    }
+  }
 }
