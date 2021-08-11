97146e37e5c390814b3e652f6d9fbaef1d5aee2e
Konstantin Pomazan <kospomazan@gmail.com>
2021-08-11 19:44:22 +0300

    added carousel's arrows handler

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,38 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".carousel");
+  const carouselInner = carousel.querySelector(".carousel__inner");
+  const slide = carousel.querySelector(".carousel__slide");
+  const arrowLeft = carousel.querySelector(".carousel__arrow_left");
+  const arrowRight = carousel.querySelector(".carousel__arrow_right");
+  const slideItems = carousel.querySelectorAll(".carousel__slide");
+
+  let currentSlide = 0;
+  let slideWidth = slide.offsetWidth;
+
+  handlerArrows();
+
+  const rollSlider = () => carouselInner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+
+  function handlerArrows() {
+    arrowLeft.style.display = "";
+    arrowRight.style.display = "";
+
+    if (currentSlide <= 0) {
+      arrowLeft.style.display = "none";
+    } else if (currentSlide >= slideItems.length - 1) {
+      arrowRight.style.display = "none";
+    }
+  }
+
+  arrowLeft.addEventListener("click", () => {
+    currentSlide--;
+    handlerArrows();
+    rollSlider();
+  });
+
+  arrowRight.addEventListener("click", () => {
+    currentSlide++;
+    handlerArrows();
+    rollSlider();
+  });
 }
